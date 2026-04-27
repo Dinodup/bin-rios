54ac4dfb27a700ad63ed1719567b4f0acb176269
Dinodup <103062188+Dinodup@users.noreply.github.com>
2026-04-27 13:23:25 -0300

    Função contazero e incremento na Função operação
    Tendo a função contazero, é possível que os printf mostrem os zeros a esquerda sem que pulem direto para os ums
    
    Foi iniciada a função operação em que por enquanto retira os dígitos da direita dos 3 binários, no futuro serão feitas as devidas operações para cada parte desses dígitos

--- main.c
+++ main.c
@@ -9,8 +9,32 @@ Pedro Henrique Alves da Silva
 #include <stdio.h>
 
 /* Função que soma os 3 binários com uso das operações */
-int binf(int bin1, int bin2, int bin3, int mm1, int mm2){
-    int somabin;
+int operacao(int bin1, int bin2, int bin3, int mm1, int mm2){
+    int somabin, dig1, dig2, dig3;
+
+    for(int i = 0; i<8; i++){
+        dig1 = bin1 % 10;
+        if(dig1 == 1){
+            bin1--;
+        }
+        bin1 = bin1 / 10;
+        printf("digito 1: %d\n", dig1);
+        
+        dig2 = bin2 % 10;
+        if(dig2 == 1){
+            bin2--;
+        }
+        bin2 = bin2 / 10;
+        printf("digito 2: %d\n", dig2);
+        
+        dig3 = bin3 % 10;
+        if(dig3 == 1){
+            bin3--;
+        }
+        bin3 = bin3 / 10;
+        printf("digito 3: %d\n\n", dig3);
+        
+    }
     
     /* Não pode simplesmente converter os 
     binários para inteiros correspondentes,
@@ -35,6 +59,17 @@ int bin_hex(int bin){
     para símbolos hexadecimais. */
     
     return hexa;
+}
+
+/* Função que coloca os zeros no printf evitando que apareçam somente números um */
+int contazero(int bin){
+    int resto;
+    for(int i = 10000000; i>0; i = i / 10){
+        resto = bin / i;
+        if(resto == 0){
+            printf("%d", resto);
+        }
+    }
 }
 
 int main()
@@ -69,12 +104,16 @@ int main()
         com sinal correspondente, em complemento a 2, 
         assim como também para hexadecimal. */
         
+        contazero(bin1);
         printf("\n%db (%di %dh)\n", bin1, inte1, hexa1);
         printf("%c\n", mm1);
+        contazero(bin2);
         printf("%db (%di %dh)\n", bin2, inte2, hexa2);
         printf("%c\n", mm2);
+        contazero(bin3);
         printf("%db (%di %dh)\n", bin3, inte3, hexa3);
         printf("=\n");
+        contazero(binF);
         printf("%db (%di %dh)\n\n", binF, inteF, hexaF);
         
         scanf("%d", &bin1);
